c48271c65a3cfa4620a84576a8a9c5e4c6c92f41
Domange <backinsmail@gmail.com>
2019-07-21 19:39:08 +0200

    Integration complete navbar

--- client/src/components/Logout/index.js
+++ client/src/components/Logout/index.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import { AuthContext } from "../Auth/AuthProvider";
 import { Redirect } from 'react-router-dom';
 
-const Logout = () => {
+const Logout = (classname) => {
   let { setUser } = useContext(AuthContext);
 
   const onClick = () => {
@@ -10,7 +10,7 @@ const Logout = () => {
     setUser(null);
     return <Redirect to='/'/>
   };
-  return ( <a href="#" onClick={() => onClick()}>Logout</a> );
+  return ( <a href="#" className={classname.classname} onClick={() => onClick()}>Logout</a> );
 };
 
 export default Logout;
--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,9 +1,10 @@
 import React, { useContext }  from 'react';
-import { BrowserRouter , Link } from "react-router-dom";
+import { BrowserRouter , NavLink } from "react-router-dom";
 import Router from '../Router';
 import { AuthContext } from "../Auth/AuthProvider";
 import Logout from '../Logout';
 import { makeStyles } from '@material-ui/core/styles';
+import { flexbox } from '@material-ui/system';
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -19,17 +20,44 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: '#f1c40f'
   },
   item: {
-    transition: 'background-color .3s ease',
-    '&:hover': {
-      backgroundColor: 'rgba(0,0,0,0.1)'
-    }
+    display: 'flex'
   },
   itemLink: {
     display: 'block',
     padding: '1rem 0.5rem',
     textDecoration: 'none',
-    width: '100%',
-    height: '100%',
+    color: "#000",
+    fontWeight: '600',
+    borderBottom: '3px solid transparent',
+    transition: 'background-color .3s ease, border .3s ease',
+    '&:hover': {
+      borderBottom: '3px solid black',
+      backgroundColor: 'rgba(0,0,0,0.1)'
+    }
+  }, 
+  itemLinkActive: {
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    borderBottom: '3px solid black'
+  }, 
+  itemLogout: {
+    display: 'flex',
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }, 
+  itemLinkLogout: {
+    display: 'flex',
+    backgroundColor: '#34495e',
+    color: '#FFF',
+    borderRadius: '0.3rem',
+    marginLeft: '5rem',
+    alignItems: 'center',
+    padding: '.5rem',
+    textDecoration: 'none',
+    transition: 'background-color .3s ease',
+    '&:hover': {
+      backgroundColor: '#2c3e50',
+    }
   }
 }));
 
@@ -44,21 +72,21 @@ const NavBar = () => {
         <nav>
           <ul className={classes.menu}>
             <li className={classes.item}>
-              <Link className={classes.itemLink} to="/">Home</Link>
+              <NavLink className={classes.itemLink} to="/" activeClassName={classes.itemLinkActive}>Home</NavLink>
             </li>
-            <li className={classes.item}><Link className={classes.itemLink} to="/search">Search</Link></li>
+            <li className={classes.item}><NavLink className={classes.itemLink} to="/search" activeClassName={classes.itemLinkActive}>Search</NavLink></li>
             {
               user ? <>
-                  <li className={classes.item}><Logout className={classes.itemLink}/></li>
-                  <li className={classes.item}><Link className={classes.itemLink} to="/create/staff">Add staff</Link></li>
-                  <li className={classes.item}><Link className={classes.itemLink} to="/create/movie">Add movie</Link></li>
+                  <li className={classes.item}><NavLink className={classes.itemLink} to="/create/staff" activeClassName={classes.itemLinkActive}>Add staff</NavLink></li>
+                  <li className={classes.item}><NavLink className={classes.itemLink} to="/create/movie" activeClassName={classes.itemLinkActive}>Add movie</NavLink></li>
+                  <li className={classes.itemLogout}><Logout classname={classes.itemLinkLogout}/></li>
                 </>:
                 <>
                   <li className={classes.item}>
-                    <Link className={classes.itemLink} to="/login">Login</Link>
+                    <NavLink className={classes.itemLink} to="/login" activeClassName={classes.itemLinkActive}>Login</NavLink>
                   </li>
                   <li className={classes.item}>
-                    <Link className={classes.itemLink} to="/signup">Sign up</Link>
+                    <NavLink className={classes.itemLink} to="/signup" activeClassName={classes.itemLinkActive}>Sign up</NavLink>
                   </li>
                 </>
             }
